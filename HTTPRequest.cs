0043544a48a6f48bb34ac59b7fc5b831e4b3ad7a
Ali yehia <161227021+ayehia8@users.noreply.github.com>
2026-03-27 20:48:00 +0200

    lab.6

---
+++ HTTPRequest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HttpClient
+{
+    class HTTPRequest
+    {
+        public static void HandleRequest(object obj)
+        {
+            string website = (string)obj;
+
+            IPAddress[] addresses;
+
+            try
+            {
+                addresses = Dns.GetHostAddresses(website);
+
+                if (addresses.Length == 0)
+                {
+                    Console.WriteLine("Error: " + website);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            IPAddress host = addresses[0];
+            int port = 80;
+
+            IPEndPoint endpoint = new IPEndPoint(host, port);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+                                       SocketType.Stream,
+                                       ProtocolType.Tcp);
+
+            socket.Connect(endpoint);
+
+            string request =
+                "GET / HTTP/1.1\r\n" +
+                "Host: " + website + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
+
+            socket.Send(Encoding.ASCII.GetBytes(request));
+
+            byte[] data = new byte[1024 * 1024];
+            int received = socket.Receive(data);
+
+            string response = Encoding.ASCII.GetString(data, 0, received);
+
+            socket.Close();
+
+            File.WriteAllText(website + ".html", response);
+
+            Console.WriteLine("Finished: " + website);
+        }
+    }
+}
---
+++ Program-lab.6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using HttpClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] websites = new string[]
+        {
+            "WWW.google.com",
+            "www.gmail.com",
+            "www.yahoo.com"
+        };
+
+        foreach (string website in websites)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(HTTPRequest.HandleRequest));
+            thread.Start(website);
+        }
+
+        Console.ReadLine(); 
+    }
+}
